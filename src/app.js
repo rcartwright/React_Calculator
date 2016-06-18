2d2c2c6e386ca2f0aed35d0561f3e051523913b8
Rachel D. Cartwright <Rachel.D.Cartwright@gmail.com>
2016-06-18 13:42:30 -0700

    create beginning structure

---
+++ src/app.js
@@ -0,0 +1,33 @@
+"use strict";
+var React = require('react');
+var render = require('react-dom').render;
+
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+    )
+  }
+})
+
+var Calculator = React.createClass({
+  getInitialState: function() {
+    return (
+      currentState: 0
+    );
+  },
+
+  render: function() {
+    return (
+      <div>
+      {this.state.currentState}
+      </div>
+    )
+  }
+}):
+
+
+render((<App />), document.getElementById("app"));
